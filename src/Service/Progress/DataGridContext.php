4db41d8c8576def97ffe0f7b94380ae0666703fe
popovsergiy <popow.sergiy@gmail.com>
2016-11-05 19:57:48 +0200

    Improve Context message

--- src/Service/Progress/DataGridContext.php
+++ src/Service/Progress/DataGridContext.php
@@ -87,29 +87,11 @@ class DataGridContext implements ObjectManagerAwareInterface, TranslatorAwareInt
     public function getMessage()
     {
         $om = $this->getObjectManager();
-        //$item = $om->find('My\Entity', 1);
-        //$item->setTitle('Changed Title!');
-
         $uow = $om->getUnitOfWork();
         $uow->computeChangeSets(); // do not compute changes if inside a listener
         $changeSet = $uow->getEntityChangeSet($item = $this->getItem());
 
-        $changedField = [];
-        foreach ($changeSet as $field => $set) {
-            if (is_int($set[0])) {
-                $set[1] = (int) $set[1];
-            } elseif (is_float($set[0])) {
-                $set[1] = (float) $set[1];
-            } elseif (is_bool($set[1])) {
-                $set[1] = (bool) $set[1];
-            }
-
-            if ($set[0] !== $set[1]) {
-                $changedField[] = $field;
-            }
-        }
-
-        $message = '';
+        $message = [];
         if (isset($changedField[0])) {
             $translator = $this->getTranslator();
             $module = $this->getModulePlugin()->setRealContext($item)->getRealModule();
@@ -118,7 +100,6 @@ class DataGridContext implements ObjectManagerAwareInterface, TranslatorAwareInt
                 ->setContext($this->getFieldsService()->getAllByEntity($entity))
                 ->asAssociate('mnemo');
 
-            $message = [];
             $message[] = $translator->translate(
                 ucfirst($entity->getMnemo()) . ' edited',
                 $module->getName(),
