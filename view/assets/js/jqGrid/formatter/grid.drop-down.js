6a30096504f907b6322e867907e266b777b70b4a
Vlad Kozak <vlad.gem.typ@gmail.com>
2018-09-26 15:41:42 +0300

    dropdown has been added

--- view/assets/js/jqGrid/formatter/grid.drop-down.js
+++ view/assets/js/jqGrid/formatter/grid.drop-down.js
@@ -1,58 +1,24 @@
 (function ($) {
   "use strict";
 
-  //let linkFormatter = new DataGrid_LinkFormatter();
-
   $.extend($.fn.fmatter, {
     dropDown: function (cellValue, options, rowData) {
       let value = $(cellValue);
 
-
-
-      let name = options.colModel.name;
-
-      // Check if value is HTML tag formatted on server
-      /*if ('<' === value.substring(0, 1)) {
-        let elm = $(value);
-        let href = elm.find('a');
-        if (!href.length && !elm.is('a')) {
-          try {
-            let row = Object.assign({}, rowData);
-            let fOptions = options.colModel.formatoptions.link;
-
-            row[name] = cellValue;
-
-            linkFormatter.setLink(':marketplace_domain:/dp/:product_asin:');
-            linkFormatter.setLinkColumnPlaceholders(fOptions.column);
-            linkFormatter.setRowData(rowData);
-            value = linkFormatter.getFormattedValue(name);
-
-
-            //console.log(value, arguments);
-          } catch(error) {
-            console.error(error);
-            // expected output: SyntaxError: unterminated string literal
-            // Note - error messages will vary depending on browser
-          }
+    cellValue = "<div class=\"dropdown\">" +
+        "<button class=\"btn btn-default dropdown-toggle\" type=\"button\" id=\"dropdownMenu1\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">"+
+        "More" +
+        "<span class=\"caret\"></span></button>"+
+        "<ul class=\"dropdown-menu\" style='position: inherit;' aria-labelledby=\"dropdownMenu1\">";
+      for (var prop in value) {
+        if ($(value[prop]).is('a')) {
+          cellValue += "<li><a target='_blank' href=" + value[prop].getAttribute('href')  + "\>" + $(value[prop]).text() + "</a></li>";
         }
-      }*/
+      }
 
-    /*<div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-        <button type="button" class="btn btn-secondary">1</button>
-        <button type="button" class="btn btn-secondary">2</button>
+      cellValue += "</ul>"+
+      "</div>";
 
-        <div class="btn-group" role="group">
-        <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
-        </button>
-        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-        <a class="dropdown-item" href="#">Dropdown link</a>
-        <a class="dropdown-item" href="#">Dropdown link</a>
-      </div>
-      </div>
-      </div>*/
-
-      console.log(value/*, arguments*/);
       return cellValue;
     }
   });
@@ -64,3 +30,4 @@
   });
 
 })(jQuery);
+
