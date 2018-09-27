71b808874727f1f608b636a38f83bdbf5c1cf0fe
Vlad Kozak <vlad.gem.typ@gmail.com>
2018-09-27 13:40:12 +0300

    Btn-xs has been added

--- view/assets/js/jqGrid/formatter/grid.drop-down.js
+++ view/assets/js/jqGrid/formatter/grid.drop-down.js
@@ -6,7 +6,7 @@
       let value = $(cellValue);
 
     cellValue = "<div class=\"dropdown\">" +
-        "<button class=\"btn btn-default dropdown-toggle\" type=\"button\" id=\"dropdownMenu1\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">"+
+        "<button class=\"btn btn-default btn-xs dropdown-toggle\" type=\"button\" id=\"dropdownMenu1\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">"+
         "More" +
         "<span class=\"caret\"></span></button>"+
         "<ul class=\"dropdown-menu\" style='position: inherit;' aria-labelledby=\"dropdownMenu1\">";
